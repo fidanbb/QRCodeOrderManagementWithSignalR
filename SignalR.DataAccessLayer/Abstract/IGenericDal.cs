757dd658a313023786988a7bc19874d8a985749e
fidanbb <fidanbb@code.edu.az>
2024-05-29 21:32:16 +0400

    Add project files.

---
+++ SignalR.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T>where T : class
+    {
+        Task Add (T entity);
+        Task Update (T entity);
+        Task Delete (T entity);
+
+        Task<List<T>> GetAll ();
+        Task<T> GetByID (int id);
+    }
+}
