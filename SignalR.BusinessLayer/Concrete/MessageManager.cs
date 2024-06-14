39f8d3333ce3244c0d4ee9cf716f9635b2c9a95c
fidanbb <fidanbb@code.edu.az>
2024-06-14 22:29:56 +0400

    added contactUs section in default page

---
+++ SignalR.BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,46 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+	public class MessageManager : IMessageService
+	{
+		private readonly IMessageDal _messageDal;
+
+		public MessageManager(IMessageDal messageDal)
+		{
+			_messageDal = messageDal;
+		}
+
+		public async Task TAdd(Message entity)
+		{
+			await _messageDal.Add(entity);
+		}
+
+		public async Task TDelete(Message entity)
+		{
+			await _messageDal.Delete(entity);
+		}
+
+		public async Task<List<Message>> TGetAll()
+		{
+			return await _messageDal.GetAll();
+		}
+
+		public async Task<Message> TGetByID(int id)
+		{
+			return await (_messageDal.GetByID(id));
+		}
+
+		public async Task TUpdate(Message entity)
+		{
+			await _messageDal.Update(entity);
+		}
+	}
+}
--- SignalRAPi/Controllers/MessageController.cs
+++ SignalRAPi/Controllers/MessageController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.MessageDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRAPi.Controllers
 {
@@ -7,5 +11,71 @@ namespace SignalRAPi.Controllers
 	[ApiController]
 	public class MessageController : ControllerBase
 	{
+		private readonly IMessageService _messageService;
+		private readonly IMapper _mapper;
+		public MessageController(IMessageService messageService,
+			                     IMapper mapper)
+		{
+			_messageService = messageService;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> MessageList()
+		{
+			var values = _mapper.Map<List<ResultMessageDto>>(await _messageService.TGetAll());
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateMessage(CreateMessageDto createMessageDto)
+		{
+			Message message = new Message()
+			{
+				Mail = createMessageDto.Mail,
+				MessageContent = createMessageDto.MessageContent,
+				MessageSendDate = DateTime.Now,
+				NameSurname = createMessageDto.NameSurname,
+				Phone = createMessageDto.Phone,
+				Status = false,
+				Subject = createMessageDto.Subject
+			};
+
+			await _messageService.TAdd(message);
+			return Ok("Message added successfully");
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteMessage(int id)
+		{
+			var value =await _messageService.TGetByID(id);
+			await _messageService.TDelete(value);
+			return Ok("Message deleted successfully");
+		}
+
+		[HttpPut]
+		public async Task<IActionResult> UpdateMessage(UpdateMessageDto updateMessageDto)
+		{
+			Message message = new Message()
+			{
+				Mail = updateMessageDto.Mail,
+				MessageContent = updateMessageDto.MessageContent,
+				MessageSendDate = updateMessageDto.MessageSendDate,
+				NameSurname = updateMessageDto.NameSurname,
+				Phone = updateMessageDto.Phone,
+				Status = false,
+				Subject = updateMessageDto.Subject,
+				MessageID = updateMessageDto.MessageID
+			};
+			await _messageService.TUpdate(message);
+			return Ok("Message updated successfully");
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetMessage(int id)
+		{
+			var value = _mapper.Map<ResultMessageDto>(await _messageService.TGetByID(id));
+			return Ok(value);
+		}
 	}
 }
